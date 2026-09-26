c9ff1d6f2403f1d117bdeccbf8c1830738efbdef
Priyasha <ST10482271@rcconnect.edu.za>
2026-09-26 12:02:40 +0200

    Added a PostResultDto for posting race results.

---
+++ RaceDay Application/DTOs/PostResultDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RaceDayApplication.DTOs
+{
+    public class PostResultDto
+    {
+        public int EnrollmentId { get; set; }
+        public int Position { get; set; }
+        public string FinishTime { get; set; } = "00:00:00"; // "HH:mm:ss"
+    }
+    // This DTO is used for posting race results.
+    // It contains the necessary properties that a client(organizer) must provide when making a request to post a race result.
+    // The properties include the enrollment ID, position, and finish time.
+    //It connects to the PostResult endpoint in the OrganizerController and is used to submit race results for a specific enrollment.
+    // It is a simple object that contains only the necessary properties for posting race results, without any additional logic or behavior.
+}
