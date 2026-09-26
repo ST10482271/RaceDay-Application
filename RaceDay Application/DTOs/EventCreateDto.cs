60f23d6637708a22b43dd2c71c02cacc67735589
Priyasha <ST10482271@rcconnect.edu.za>
2026-09-26 11:08:21 +0200

    Added a DTOs folder and created the EventCreateDto.

---
+++ RaceDay Application/DTOs/EventCreateDto.cs
@@ -0,0 +1,18 @@
+﻿namespace RaceDayApplication.DTOs
+{
+    public class EventCreateDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+    }
+    //A DTO is what the database uses to transfer data between the client and the server.
+    //It is there so only the necessary data is transferred and makes sure properties are not exposed to the client that should not be.
+    //It is a way to encapsulate data and send it over the network in a structured format.
+    // It is a simple object that contains only the necessary properties for creating an event, without any additional logic or behavior.
+
+    // This DTO is used for creating new events.
+    // It contains the necessary properties that a client must provide when making a request to create an event.
+    // The properties include the event's name, date, location, and category ID.
+}
